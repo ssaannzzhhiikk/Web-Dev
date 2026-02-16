d1528d638dbc2d939712aeaa5868cc21816fa517
Sanzhar <omarkhanovsanzh@gmail.com>
2026-02-16 23:54:35 +0500

    Interface model for products

---
+++ Lab4/Task2/kaspi-app/src/app/model/product.model.ts
@@ -0,0 +1,10 @@
+export interface productModel {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    rating: number,
+    image: string,
+    images: string[],
+    link: string
+}
