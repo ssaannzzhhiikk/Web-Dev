1b5a65c159fe01fb9ae99bfefada2f798f43357e
Sanzhar <omarkhanovsanzh@gmail.com>
2026-02-23 00:35:04 +0500

    Added like button and rubbish

--- Lab5/kaspi-app/src/app/components/product-card/product-card.component.ts
+++ Lab5/kaspi-app/src/app/components/product-card/product-card.component.ts
@@ -1,31 +1,45 @@
-import { Component, input } from "@angular/core";
+import { Component, input, output, signal } from "@angular/core";
 import { productModel } from "../../model/product.model";
 
 @Component({
     selector: 'prod-card',
-    
     imports: [],
     templateUrl: './product-card.component.html',
     styleUrl: './product-card.component.css'
 })
-export class productCard{
+export class productCard {
     readonly product = input.required<productModel>();
-    readonly Math = Math; 
+    readonly Math = Math;
 
-    shareWhatsApp(event: Event): void {
-        event.preventDefault();
-        event.stopPropagation();
-        const message = `Check out this product: ${this.product().link}`;
-        const url = `https://wa.me/?text=${encodeURIComponent(message)}`;
-        window.open(url, '_blank');
+    readonly deleteProduct = output<number>();
+
+    likes = signal(0);
+    liked = signal(false);
+
+    ngOnInit() {
+        this.likes.set(this.product().likes);
     }
 
-    shareTelegram(event: Event): void {
-        event.preventDefault();
-        event.stopPropagation();
+    onLike(): void {
+        this.likes.update(l => l + 1);
+        this.liked.set(true);
+    }
+
+    onDelete(): void {
+        this.deleteProduct.emit(this.product().id);
+    }
+
+    shareWhatsApp(): void {
+    const message = `Check out this product: ${this.product().link}`;
+    const url = `https://wa.me/?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+    }
+
+    shareTelegram(): void {
         const url = this.product().link;
         const text = this.product().name;
         const shareUrl = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}`;
         window.open(shareUrl, '_blank');
     }
 }
+
