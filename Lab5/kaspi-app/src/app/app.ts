5bbafa0b322cc723e3131d8563ca5fcbb2178082
Sanzhar <omarkhanovsanzh@gmail.com>
2026-02-23 00:34:37 +0500

    Added more logic for app component

--- Lab5/kaspi-app/src/app/app.ts
+++ Lab5/kaspi-app/src/app/app.ts
@@ -1,14 +1,38 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { productCard } from './components/product-card/product-card.component';
+import { Component } from '@angular/core';
 import { productList } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.services';
+import { categoryModel } from './model/category.model';
+import { productModel } from './model/product.model';
+import { UpperCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, productList],
+  imports: [productList, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('kaspi-app');
+  categories: categoryModel[] = [];
+  selectedCategoryId: number | null = null;
+  filteredProducts: productModel[] = [];
+
+constructor(private productService: ProductService) {
+    this.categories = this.productService.getCategories();
+    this.filteredProducts = this.productService.getProducts();
+    this.selectedCategoryId = 0;
+}
+
+selectCategory(categoryId: number): void {
+    this.selectedCategoryId = categoryId;
+    if (categoryId === 0) {
+        this.filteredProducts = this.productService.getProducts();
+    } else {
+        this.filteredProducts = this.productService.getProductsByCategory(categoryId);
+    }
+}
+
+  isSelected(categoryId: number): boolean {
+    return this.selectedCategoryId === categoryId;
+  }
 }
